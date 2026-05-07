bc8b6878f0896cd3c27718cd8bc38cf84cd02be1
MaisarahSuzaimee <104546860+MaisarahSuzaimee@users.noreply.github.com>
2026-05-07 08:02:28 +0800

    update widget jawatan

--- app/Filament/Widgets/JawatanLegend.php
+++ app/Filament/Widgets/JawatanLegend.php
@@ -6,6 +6,7 @@ use Filament\Widgets\Widget;
 
 class JawatanLegend extends Widget
 {
+    protected static bool $isDiscovered = false;
     protected string $view = 'filament.widgets.jawatan-legend';
 
     protected int | string | array $columnSpan = 'full';
