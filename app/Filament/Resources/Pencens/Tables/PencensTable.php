e79f857b075c6e3140fa01346f60c0cb4ac5e033
MaisarahSuzaimee <104546860+MaisarahSuzaimee@users.noreply.github.com>
2026-06-03 14:54:26 +0800

    softdelete for unit

--- app/Filament/Resources/Pencens/Tables/PencensTable.php
+++ app/Filament/Resources/Pencens/Tables/PencensTable.php
@@ -8,6 +8,7 @@ use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
 
 class PencensTable
 {
@@ -19,26 +20,43 @@ class PencensTable
                     ->label('Bil')
                     ->rowIndex(),
                 TextColumn::make('nama')
-                    ->label('Nama Pegawai'),
-                    // ->formatStateUsing(
-                    //     fn($record) =>
-                    //     '<strong>' . ($record->nama ?? '-') . '</strong><br>' .
-                    //     ($record->nokp ?? '-') . '<br>' .
-                    //     (
-                    //         $record->jawatan_gred
-                    //         ? $record->jawatan_gred->jawatan->desc_jawatan .
-                    //         ' (' . $record->jawatan_gred->gred->kod_gred . ')'
-                    //         : '-'
-                    //     )
-                    // )
-                    // ->html(),
-                TextColumn::make('ptj.nama_ptj'),
+                    ->label('Nama Pegawai')
+                    ->sortable()
+                    ->searchable(),
+                // ->formatStateUsing(
+                //     fn($record) =>
+                //     '<strong>' . ($record->nama ?? '-') . '</strong><br>' .
+                //     ($record->nokp ?? '-') . '<br>' .
+                //     (
+                //         $record->jawatan_gred
+                //         ? $record->jawatan_gred->jawatan->desc_jawatan .
+                //         ' (' . $record->jawatan_gred->gred->kod_gred . ')'
+                //         : '-'
+                //     )
+                // )
+                // ->html(),
+                TextColumn::make('ptj.nama_ptj')
+                    ->label('PTJ')
+                    ->sortable()
+                    ->searchable(),
+
                 TextColumn::make('tarikh')
                     ->label('Tarikh Pencen / Kuatkuasa')
-                    ->getStateUsing(function ($record) {
-                        return $record->tarikh_kuatkuasa ?? $record->tarikh_pencen;
-                    })
+                    ->getStateUsing(fn($record) => $record->tarikh_kuatkuasa ?? $record->tarikh_pencen)
                     ->date('d F Y')
+                    ->sortable(query: function (Builder $query, string $direction): Builder {
+                        return $query->orderByRaw("
+                                                    COALESCE(tarikh_kuatkuasa, tarikh_pencen) {$direction}
+                                                ");
+                    })
+                    ->searchable(
+                        query: function (Builder $query, string $search): Builder {
+                            return $query->where(function ($query) use ($search) {
+                                $query->where('tarikh_kuatkuasa', 'like', "%{$search}%")
+                                    ->orWhere('tarikh_pencen', 'like', "%{$search}%");
+                            });
+                        }
+                    ),
 
             ])
             ->filters([
