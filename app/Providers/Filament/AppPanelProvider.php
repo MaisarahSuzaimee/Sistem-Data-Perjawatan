7b0b21aa1b220c815f2683557fb2081a4dfe8191
MaisarahSuzaimee <104546860+MaisarahSuzaimee@users.noreply.github.com>
2026-06-03 11:05:44 +0800

    test theme

--- app/Providers/Filament/AppPanelProvider.php
+++ app/Providers/Filament/AppPanelProvider.php
@@ -19,6 +19,7 @@ use Illuminate\Foundation\Http\Middleware\PreventRequestForgery;
 use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\StartSession;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
+use Caresome\FilamentNeobrutalism\NeobrutalismeTheme;
 
 class AppPanelProvider extends PanelProvider
 {
@@ -32,7 +33,19 @@ class AppPanelProvider extends PanelProvider
             ->viteTheme('resources/css/filament/app/theme.css')
             ->login()
             ->colors([
-                'primary' => Color::Amber,
+                'primary' => [
+                    50 => '#fbf6ef',
+                    100 => '#f3e6d3',
+                    200 => '#e3c8a6',
+                    300 => '#d0a672',
+                    400 => '#b88246',
+                    500 => '#4d2e07', // your main color
+                    600 => '#3f2506',
+                    700 => '#321d05',
+                    800 => '#241504',
+                    900 => '#170d02',
+                    950 => '#0c0701',
+                ],
                 'secondary' => Color::Violet,
                 'tertiary' => Color::Lime,
             ])
@@ -46,7 +59,7 @@ class AppPanelProvider extends PanelProvider
                 AccountWidget::class,
                 FilamentInfoWidget::class,
             ])
-           
+
             ->middleware([
                 EncryptCookies::class,
                 AddQueuedCookiesToResponse::class,
@@ -60,7 +73,13 @@ class AppPanelProvider extends PanelProvider
             ])
             ->authMiddleware([
                 Authenticate::class,
-            ]);
+            ])
+            ->plugin(NeobrutalismeTheme::make()
+                ->customize([
+                    '--my-custom-color' => '#008080',
+                    '--my-custom-spacing' => '2rem',
+                ]));
+
 
     }
 }
